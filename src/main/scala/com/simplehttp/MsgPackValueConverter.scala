953e5553cbc952044c5f493bfe453291a1c974a2
Cosmin Basca <cosmin.basca@gmail.com>
2014-07-29 12:23:21 +0000

    simplehttp version 0.1.7
    - simplified the msgpack converter trait

---
+++ src/main/scala/com/simplehttp/MsgPackValueConverter.scala
@@ -0,0 +1,44 @@
+package com.simplehttp
+
+import com.typesafe.scalalogging.slf4j.Logger
+import org.msgpack.`type`.Value
+import org.slf4j.LoggerFactory
+
+/**
+  * simple [[http://msgpack.org/ MessagePack]] value caster
+  * @tparam T the type of the converted value
+  */
+trait MsgPackValueConverter[T] {
+   /**
+    * a logger
+    */
+   val logger = Logger(LoggerFactory getLogger "ValueConverter")
+
+   /**
+    * convert the given [[http://msgpack.org/ MessagePack]] value
+    * @param value the value
+    * @return the converted value of type [[T]]
+    */
+   protected def convert(value: Value): T
+
+  /**
+   * convert the given [[http://msgpack.org/ MessagePack]] value
+   * @param value the value
+   * @return the converted value of type [[T]]
+   */
+  def apply(value: Value): T = {
+    convert(value)
+  }
+
+  /**
+   * convert the given [[http://msgpack.org/ MessagePack]] optional value
+   * @param value the optional value
+   * @return the converted value of type [[T]] of None, if no value was given
+   */
+  def apply(value: Option[Value]): Option[T] = {
+    value match {
+      case Some(concreteValue) => Some(convert(concreteValue))
+      case None => None
+    }
+  }
+ }
