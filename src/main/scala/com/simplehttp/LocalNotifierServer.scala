b8a8d2459bee9c1ab37e8cdc51f8d38937774778
Cosmin Basca <cosmin.basca@gmail.com>
2014-09-12 11:55:30 +0000

    simplehttp version 0.2.6
    - async application container is in 
    - port notification is returned with a prefix ... (so that the line can be identified easily ... )

--- src/main/scala/com/simplehttp/LocalNotifierServer.scala
+++ src/main/scala/com/simplehttp/LocalNotifierServer.scala
@@ -65,41 +65,43 @@ abstract class LocalNotifierServer[T, U <: ApplicationContainer[T]] extends App 
     }
   }
 
-  val parser = new scopt.OptionParser[ServerConfig]("localNotifierServer") {
-    head(BuildInfo.name, BuildInfo.version)
+  override def main(args: Array[String]) = {
+    val parser = new scopt.OptionParser[ServerConfig]("localNotifierServer") {
+      head(BuildInfo.name, BuildInfo.version)
 
-    opt[Int]('p', "port") action {
-      (x, c) => c.copy(port = x)
-    } text "port is the port to bind to"
+      opt[Int]('p', "port") action {
+        (x, c) => c.copy(port = x)
+      } text "port is the port to bind to"
 
-    opt[Unit]("die_on_broken_pipe") action {
-      (_, c) => c.copy(dieOnBrokenPipe = true)
-    } text "if set to true (default) the server will exit when the parent starting process exists"
+      opt[Unit]("die_on_broken_pipe") action {
+        (_, c) => c.copy(dieOnBrokenPipe = true)
+      } text "if set to true (default) the server will exit when the parent starting process exists"
 
-    opt[String]('n', "port_notification_prefix") action {
-      (x, c) => c.copy(portNotificationPrefix = x)
-    } text "the port notification prefix used (to detect the port reporting line)"
+      opt[String]('n', "port_notification_prefix") action {
+        (x, c) => c.copy(portNotificationPrefix = x)
+      } text "the port notification prefix used (to detect the port reporting line)"
 
-    opt[Int]('n', "num_threads") action {
-      (x, c) => c.copy(numThreads = x)
-    } text "the number of threads to allocate for the worker pool (default=10)"
+      opt[Int]('n', "num_threads") action {
+        (x, c) => c.copy(numThreads = x)
+      } text "the number of threads to allocate for the worker pool (default=10)"
 
-    opt[Unit]("asynchronous") action {
-      (_, c) => c.copy(asynchronous = true)
-    } text "if set to true (default is false) the context is wrapped by an asynchronous context"
+      opt[Unit]("asynchronous") action {
+        (_, c) => c.copy(asynchronous = true)
+      } text "if set to true (default is false) the context is wrapped by an asynchronous context"
 
-    opt[Int]('n', "num_async_workers") action {
-      (x, c) => c.copy(numAsyncWorkers = x)
-    } text "the number of async worker threads to allocate for the async executor (default=10), this parameter is ignored if asynchronous is false"
+      opt[Int]('n', "num_async_workers") action {
+        (x, c) => c.copy(numAsyncWorkers = x)
+      } text "the number of async worker threads to allocate for the async executor (default=10), this parameter is ignored if asynchronous is false"
 
-    arg[String]("<other>...") unbounded() optional() action {
-      (x, c) => c.copy(other = c.other :+ x)
-    } text "optional unbounded args"
-  }
+      arg[String]("<other>...") unbounded() optional() action {
+        (x, c) => c.copy(other = c.other :+ x)
+      } text "optional unbounded args"
+    }
 
-  parser.parse(args, ServerConfig()) map { config =>
-    serveForever(config.port, config.dieOnBrokenPipe, config.portNotificationPrefix, config.numThreads, config.asynchronous, config.numAsyncWorkers, config.other)
-  } getOrElse {
-    println("arguments could not be parsed.")
+    parser.parse(args, ServerConfig()) map { config =>
+      serveForever(config.port, config.dieOnBrokenPipe, config.portNotificationPrefix, config.numThreads, config.asynchronous, config.numAsyncWorkers, config.other)
+    } getOrElse {
+      println("arguments could not be parsed.")
+    }
   }
 }
